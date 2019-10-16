30c2a58c16abe698a836b957d65f523e2f9c5a2a
Frank Eggen <eggen@emden.net>
2019-10-16 12:55:54 +0200

    Add AndroidBw Stuff

--- device.mk
+++ device.mk
@@ -39,6 +39,15 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_tuning_mixer.txt:$(TARGET_COPY_OUT_VENDOR)/etc/audio_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml
+	
+# AndroidBw
+PRODUCT_PACKAGES += \
+    firefox \
+    signal \
+    osmand \
+    vlc \
+#    fakegps \
+    bwcommunity
 
 # Keylayouts
 PRODUCT_COPY_FILES += \
